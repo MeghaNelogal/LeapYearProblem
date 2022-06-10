dde7a0513b5c86ee825f48211c2a46a2acbd9e4e
Your Name <you@example.com>
2022-06-10 11:36:42 +0530

    [Megha] Add: Leap Year is Done

---
+++ LeapYear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeapYear
+{
+    public class program
+    {
+        public static void Main(string[] args )
+        {
+            LeapYears leap = new LeapYears();
+            leap.LeapYear();
+        }
+    }
+}
